30a78957258f84cb7104a4adf02d555e220726b9
katocalvin10 <katocalvin10@gmail.com>
2026-03-30 15:36:14 +0300

    Added LogbookEntryModel

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class LogbookEntry(models.Model):
+    activity = models.TextField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.activity
