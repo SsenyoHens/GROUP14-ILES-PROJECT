eb1feda46c73aac66039af6cd3d2fea6b1d1afaa
Ivanthe-eng <kyanziivan85@gmail.com>
2026-04-17 20:01:33 +0300

    added app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,31 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import Login from './pages/Login'
+import Dashboard from './pages/admin/Dashboard'
+import Students from './pages/admin/Students'
+import Placements from './pages/admin/Placements'
+import Evaluations from './pages/admin/Evaluations'
+import Reports from './pages/admin/Reports'
+import UserAccounts from './pages/admin/UserAccounts'
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        {/* Routes will be added as features merge in */}
+        {/* Public */}
+        <Route path="/" element={<Login />} />
+
+        {/* Protected */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/placements" element={<Placements />} />
+          <Route path="/evaluations" element={<Evaluations />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/users" element={<UserAccounts />} />
+        </Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   )
 }
 
