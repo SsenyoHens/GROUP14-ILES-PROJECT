9ac15831e922ccae3c1ab75675892da78e77d578
Ivanthe-eng <kyanziivan85@gmail.com>
2026-04-17 20:05:23 +0300

    sdded app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,31 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute'
+import Login from './pages/Login'
+import Dashboard from './pages/admin/Dashboard'
+import Students from './pages/admin/Students'
+import Placements from './pages/admin/Placements'
+import Evaluations from './pages/admin/Evaluations'
+import Reports from './pages/admin/Reports'
+import UserAccounts from './pages/admin/UserAccounts'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <main>
-        <Routes>
-          {/* Routes will be added as features merge in */}
-        </Routes>
-      </main>
-      <Footer />
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        {/* Public */}
+        <Route path="/" element={<Login />} />
+
+        {/* Protected */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/students" element={<Students />} />
+          <Route path="/placements" element={<Placements />} />
+          <Route path="/evaluations" element={<Evaluations />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/users" element={<UserAccounts />} />
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
