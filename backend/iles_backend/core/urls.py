a11f95360cee6e975b80e07275c9bc1461898a2f
SsenyoHens <ssenyondohenry644@gmail.com>
2026-04-20 00:35:25 +0300

    feat(urls): updated main urls to include JWT, refresh & access tokens.

--- backend/iles_backend/core/urls.py
+++ backend/iles_backend/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import login_view,register_view, get_current_user
+from .views import login_view,register_view, get_current_user,update_student_profile
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('register/', register_view, name='register'),
     path('user/', get_current_user, name='get_current_user'),
+    path('student-profile/', update_student_profile, name='update_student_profile'),
 ]
