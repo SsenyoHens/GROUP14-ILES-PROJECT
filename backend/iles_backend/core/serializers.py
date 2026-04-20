87c87ed2a3c959a055a51ce113b687a10cfb9b18
NSG1414 <namazzigloria41@gmail.com>
2026-04-20 19:57:54 +0300

    Refractor serializers: Add PlacementSerializers with validaion and remove old serializer

--- backend/iles_backend/core/serializers.py
+++ backend/iles_backend/core/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from .models import InternshipPlacement
 
-class InternshipPlacementSerializer(serializers.ModelSerializer):
+
+class PlacementSerializer(serializers.ModelSerializer):
     class Meta:
         model = InternshipPlacement
         fields = '__all__'
 
     def validate(self, data):
-        if data['start_date'] > data['end_date']:
+        start = data.get('start_date')
+        end = data.get('end_date')
+
+        if start and end and start > end:
             raise serializers.ValidationError("Start date cannot be after end date.")
+
         return data
