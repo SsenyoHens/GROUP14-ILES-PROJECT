fa553457cc357647c0f8dd0b4f98b16490fad16b
Ivanthe-eng <kyanziivan85@gmail.com>
2026-04-21 08:03:58 +0300

    modified app.jsx main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ChakraProvider } from '@chakra-ui/react'
+import { AuthProvider } from './context/AuthContext'
+import App from './App'
+import theme from './theme'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ChakraProvider>
+  </StrictMode>
 )
