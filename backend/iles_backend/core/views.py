02f6838ed8901f2a6a080b241cd99f3baf3d05c0
SsenyoHens <ssenyondohenry644@gmail.com>
2026-04-22 12:53:01 +0300

    Add validation, status control, and deadline enforcement to WeeklyLog model, updated .gitignore, and ensured week numbers are between 1 and 52 inclusively.

--- backend/iles_backend/core/views.py
+++ backend/iles_backend/core/views.py
@@ -1,7 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import InternshipPlacement
+from .models import InternshipPlacement, WeeklyLog
 from .serializers import PlacementSerializer
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+
 
 @api_view(['POST'])
 def create_placement(request):
@@ -32,4 +35,20 @@ def update_placement(request, pk):
         serializer.save()
         return Response(serializer.data)
 
-    return Response(serializer.errors, status=400)    
+    return Response(serializer.errors, status=400) 
+
+
+@api_view(['PUT', 'PATCH']) 
+@permission_classes([IsAuthenticated]) 
+def update_weekly_log(request, pk):
+
+    try:
+        log = WeeklyLog.objects.get(pk=pk, student=request.user) 
+    except WeeklyLog.DoesNotExist:
+        return Response({"error": "Log not found"}, status=404)
+    
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    
+    return Response(serializer.errors, status=400)  
