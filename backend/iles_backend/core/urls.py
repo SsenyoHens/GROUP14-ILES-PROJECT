f52824c901597bc0220758791739c23259b92421
NSG1414 <namazzigloria41@gmail.com>
2026-04-23 10:22:02 +0300

    Update URLs and finalize log API endpoints

--- backend/iles_backend/core/urls.py
+++ backend/iles_backend/core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import create_placement, view_placements, update_placement
+from .views import register_user, login_user
 from .views import (
     create_placement, view_placements, update_placement,
     create_log, view_logs, update_log, delete_log
@@ -8,6 +9,8 @@ urlpatterns = [
     path('placements/', view_placements),
     path('placements/create/', create_placement),
     path('placements/update/<int:pk>/', update_placement),
+    path('register/', register_user),     #for email login
+    path('login/', login_user),
     
     #ADDED
     path('logs/', view_logs),
