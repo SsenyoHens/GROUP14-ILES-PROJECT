eefd3c178fe8fda50ccc8cbbfe0135956fec4d03
SsenyoHens <ssenyondohenry644@gmail.com>
2026-04-26 12:02:08 +0300

    Modified admin.py by adding WeeklyLogAdmin & WeeklyLog models.

--- backend/iles_backend/core/admin.py
+++ backend/iles_backend/core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from django import forms
+from .models import WeeklyLog, WeeklyLogHistory, CustomUser, InternshipPlacement, EvaluationCriteria, Evaluation    
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser, WeeklyLog, EvaluationCriteria, Evaluation, InternshipPlacement
-from .forms import CustomUserCreationForm
+from .models import CustomUser, InternshipPlacement, WeeklyLog, EvaluationCriteria, Evaluation
 
 
 class CustomUserAdmin(UserAdmin):
@@ -37,9 +38,50 @@ class EvaluationAdmin(admin.ModelAdmin):
     list_display = ('student', 'weekly_log', 'evaluator', 'created_at')
     search_fields = ('student__first_name', 'student__last_name')
 
+class WeeklyLogAdminForm(forms.ModelForm):
+    class Meta:
+        model = WeeklyLog
+        fields = '__all__'
+    
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)  # Extract request from kwargs
+        super().__init__(*args, **kwargs)
 
-admin.site.register(CustomUser, CustomUserAdmin)
+    def clean(self):
+        cleaned_data = super().clean()
+
+        if self.instance:
+            self.instance._current_user = self.request.user  # Set current user for validation
+        return cleaned_data
+        
+       
+            
+class WeeklyLogAdmin(admin.ModelAdmin):
+    form = WeeklyLogAdminForm
+    
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+
+        class FormWithRequest(form):
+            def __init__(self2, *args, **kwargs2):
+                kwargs2['request'] = request
+                super().__init__(*args, **kwargs2)
+
+        return FormWithRequest
+    def save_model(self, request, obj, form, change):
+        obj._current_user = request.user #inject user into model instance
+        super().save_model(request, obj, form, change)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "student":
+            kwargs["queryset"] = CustomUser.objects.filter(role="student")
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+
+aadmin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(WeeklyLog, WeeklyLogAdmin)
-admin.site.register(EvaluationCriteria)
 admin.site.register(Evaluation, EvaluationAdmin)
+admin.site.register(EvaluationCriteria)
 admin.site.register(InternshipPlacement)
+admin.site.register(WeeklyLogHistory)
