976824c4b2aa427ee8ca74500cc505af41287750
NSG1414 <namazzigloria41@gmail.com>
2026-06-02 14:23:17 +0300

    Synchronized requirements.txt

--- backend/iles_backend/core/models.py
+++ backend/iles_backend/core/models.py
@@ -174,7 +174,7 @@ class WeeklyLog(models.Model):
         unique_together = ('student', 'week_number')
         constraints = [
             models.CheckConstraint(
-                condition=Q(week_number__gte=1) & Q(week_number__lte=52),
+                check=Q(week_number__gte=1) & Q(week_number__lte=52),
                 name='week_number_valid_range',
             )
         ]
