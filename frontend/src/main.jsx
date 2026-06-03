4bdac1c364341548ffa6a3e1edd54217da37d331
NSG1414 <namazzigloria41@gmail.com>
2026-06-03 22:14:16 +0300

    Debuging

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { AuthProvider } from './context/AuthContext';
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+  <ChakraProvider theme={theme}>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </ChakraProvider>
+);
