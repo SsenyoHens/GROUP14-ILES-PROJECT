588cd7703275e7111b6c584686c1a0040fa49b94
NSG1414 <namazzigloria41@gmail.com>
2026-06-05 16:55:04 +0300

    Fixing Bugs

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -16,11 +16,14 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (res) => res,
   (err) => {
-    if (err.response?.status === 401) {
-      localStorage.removeItem('access')   
-      localStorage.removeItem('refresh')  
-      window.location.href = '/login'
-    }
+    if (
+		err.response?.status === 401 &&
+		localStorage.getItem('access')
+    ) 	{
+		localStorage.removeItem('access')
+		localStorage.removeItem('refresh')
+		window.location.href = '/login'
+	}
     return Promise.reject(err)
   }
 )
