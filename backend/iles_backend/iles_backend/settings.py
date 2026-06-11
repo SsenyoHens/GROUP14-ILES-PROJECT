f05fca0b1c1bef4847c78d546f764bffac667833
SsenyoHens <ssenyondohenry644@gmail.com>
2026-06-12 00:13:09 +0300

    Allow Vercel frontend in CORS settings

--- backend/iles_backend/iles_backend/settings.py
+++ backend/iles_backend/iles_backend/settings.py
@@ -72,7 +72,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
     
-    "https://your-vercel-app.vercel.app",
+    "https://group-14-iles-project-5uw1.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True           
