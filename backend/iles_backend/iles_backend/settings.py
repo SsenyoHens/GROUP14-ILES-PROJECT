40274e58c5ed543579bd734385f6cc4b14e38f2d
SsenyoHens <ssenyondohenry644@gmail.com>
2026-06-12 00:53:37 +0300

    Enabled cors_allow_all_origins = True for production testing

--- backend/iles_backend/iles_backend/settings.py
+++ backend/iles_backend/iles_backend/settings.py
@@ -64,7 +64,8 @@ MIDDLEWARE = [
 ]
 
 
-CORS_ALLOW_ALL_ORIGINS = False          
+#CORS_ALLOW_ALL_ORIGINS = False #This is the must be set to False in production, but can be set to True during development for ease of testing.
+CORS_ALLOW_ALL_ORIGINS = True         
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
