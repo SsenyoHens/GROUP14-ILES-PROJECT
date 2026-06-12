94d41691a25280a2f4e4373a5a58867672294afa
SsenyoHens <ssenyondohenry644@gmail.com>
2026-06-12 12:10:10 +0300

    modified settings.py to resole cors-headers access denial

--- backend/iles_backend/iles_backend/settings.py
+++ backend/iles_backend/iles_backend/settings.py
@@ -67,14 +67,14 @@ MIDDLEWARE = [
 #CORS_ALLOW_ALL_ORIGINS = False #This is the must be set to False in production, but can be set to True during development for ease of testing.
 CORS_ALLOW_ALL_ORIGINS = True         
 
-'''CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",            
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
     
     "https://group-14-iles-project-5uw1.vercel.app",
-]'''
+]
 
 CORS_ALLOW_CREDENTIALS = True           
 
