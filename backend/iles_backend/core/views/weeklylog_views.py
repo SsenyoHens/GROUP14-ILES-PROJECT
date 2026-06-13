88275e76f0759a8078bf0ef95be526e7498fcea5
NSG1414 <namazzigloria41@gmail.com>
2026-06-13 16:49:27 +0300

    Updated weekly log views to restrict approval of draft logs

--- backend/iles_backend/core/views/weeklylog_views.py
+++ backend/iles_backend/core/views/weeklylog_views.py
@@ -79,7 +79,6 @@ def update_log(request, pk):
         return Response(serializer.data)
     return Response(serializer.errors, status=400)
     
-
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
 def review_log(request, pk):
@@ -136,8 +135,6 @@ def review_log(request, pk):
         "review_status": log.review_status,
         "supervisor_comment": log.supervisor_comment,
     })
-
-
 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
